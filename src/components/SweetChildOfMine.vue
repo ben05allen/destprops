92a724c72a83abcdaade2f83d9ed3916ecfe421e
ben05allen <ben05allen@hotmail.com>
2023-07-29 14:38:57 +0900

    playing around

---
+++ src/components/SweetChildOfMine.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  msg: string
+}
+
+// const { msg = 'hello world' } = defineProps<Props>()
+const _ = defineProps<Props>()
+</script>
+
+<template>
+  <div style="background-color: aquamarine">
+    <h2>hi from sweet child of mine</h2>
+    <!-- <input type="text" v-model="propMsg" /> -->
+    <p>{{ msg }}</p>
+  </div>
+</template>
---
+++ src/components/TheParent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import SweetChildOfMine from './SweetChildOfMine.vue'
+
+const msg = ref<string>('')
+
+const changeMessage = () => {
+  msg.value = 'children are to be seen and not heard'
+}
+</script>
+
+<template>
+  <div>
+    <h2>Hi from parent</h2>
+    <input type="text" v-model="msg" />
+    <SweetChildOfMine :msg="msg" />
+    <button @click="changeMessage">Change Message</button>
+    <p>
+      {{ msg }}
+    </p>
+  </div>
+</template>
