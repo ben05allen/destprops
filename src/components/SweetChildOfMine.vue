8dad5c6f624e342f2bdeafe5ccdfe2c400fa7d0b
ben05allen <ben05allen@hotmail.com>
2023-07-30 09:31:28 +0900

    adding watch

--- src/components/SweetChildOfMine.vue
+++ src/components/SweetChildOfMine.vue
@@ -1,16 +1,33 @@
+<!-- eslint-disable vue/no-setup-props-destructure -->
 <script setup lang="ts">
+import { watch } from 'vue'
+
 interface Props {
-  msg: string
+  count: number
 }
 
-// const { msg = 'hello world' } = defineProps<Props>()
-const _ = defineProps<Props>()
+// this fails for watching count
+// defineProps<Props>()
+
+// destructuring means a watcher can be used
+const { count = 0 } = defineProps<Props>()
+
+const evenOrOdd = (num: number): string => {
+  return num % 2 === 0 ? 'even' : 'odd'
+}
+
+watch(
+  () => count,
+  () => {
+    console.log(count)
+  }
+)
 </script>
 
 <template>
   <div style="background-color: aquamarine">
-    <h2>hi from sweet child of mine</h2>
+    <h2>hi from child</h2>
     <!-- <input type="text" v-model="propMsg" /> -->
-    <p>{{ msg }}</p>
+    <p>{{ count }} is {{ evenOrOdd(count) }}</p>
   </div>
 </template>
--- src/components/TheParent.vue
+++ src/components/TheParent.vue
@@ -2,21 +2,21 @@
 import { ref } from 'vue'
 import SweetChildOfMine from './SweetChildOfMine.vue'
 
-const msg = ref<string>('')
+const count = ref<number>(0)
 
-const changeMessage = () => {
-  msg.value = 'children are to be seen and not heard'
+const increaseCount = () => {
+  count.value++
 }
 </script>
 
 <template>
   <div>
     <h2>Hi from parent</h2>
-    <input type="text" v-model="msg" />
-    <SweetChildOfMine :msg="msg" />
-    <button @click="changeMessage">Change Message</button>
+    <input type="number" v-model="count" />
+    <SweetChildOfMine :count="count" />
+    <button @click="increaseCount">Increase Count</button>
     <p>
-      {{ msg }}
+      {{ count }}
     </p>
   </div>
 </template>
